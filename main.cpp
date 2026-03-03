64f72f6d582e3db194686f9eb72bb791a889a565
Alastor20 <nik_d.007@mail.ru>
2026-03-03 16:39:32 +0300

    big rework

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <cstddef>
-#include <iterator>
+
+size_t valid_index(size_t sa, const size_t *sai, const size_t *sizes)
+{
+  for (size_t j = 0; j < sa; ++j) {
+    if (sizes[j] < sai[j]) {
+      return j;
+    }
+  }
+  return sa;
+}
 
 template < class T > T *merge(const T *const *a, size_t sa, const size_t *sai, T *c)
 {
-  size_t totalSize = std::size(c);
-  size_t *sizes = new size_t[sa];
-  for (size_t i = 0; i < totalSize; ++i) {
-    size_t index = 0;
+  size_t *sizes = new size_t[sa]();
+  size_t minId = valid_index(sa, sai, sizes), i = 0;
+  while (minId != sa) {
     for (size_t j = 0; j < sa; ++j) {
       if (sizes[j] < sai[j]) {
-        if (a[index][sizes[index]] > a[j][sizes[j]]) { // T::operator<
-          index = j;
+        if (a[minId][sizes[minId]] > a[j][sizes[j]]) { // T::operator<
+          minId = j;
         }
       }
     }
-    c[i] = a[index][sizes[index]]; // T::T(const T&)
-    ++sizes[index];
+    c[i++] = a[minId][sizes[minId]]; // T::T(const T&)
+    ++sizes[minId];
+    minId = valid_index(sa, sai, sizes);
   }
   delete[] sizes;
   return c;
