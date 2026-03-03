c8d89f6dd3446e87697aa2cba66faa0912133ea4
Alastor20 <nik_d.007@mail.ru>
2026-03-03 09:01:27 +0300

    implement merge

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
+#include <cstddef>
+#include <iterator>
+
+template < class T > T *merge(const T *const *a, size_t sa, const size_t *sai, T *c)
+{
+  size_t size = std::size(c);
+  size_t *szs = new size_t[sa];
+  for (size_t i = 0; i < size; ++i) {
+    size_t idx = 0;
+    T min = a[0][szs[0]];
+    for (size_t j = 0; j < sa; ++j) {
+      if (szs[j] < sai[j]) {
+        if (min > a[j][szs[j]]) {
+          min = a[i][szs[j]];
+          idx = j;
+        }
+      }
+    }
+    c[i] = min;
+    ++szs[idx];
+  }
+  return c;
+}
+
 int main()
 {}
